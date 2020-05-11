9c3b7e297980482a8cbf4e64c0177016c6e6bb96
Vrushab R <30525467+DEV-VRUPER@users.noreply.github.com>
2020-05-11 11:55:21 +0100

    Update Selenium_topcash.py

--- Selenium_topcash.py
+++ Selenium_topcash.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 userStr = 'your_ID'
 passStr = 'your_Password'
 browser = webdriver.Chrome()
-browser.get('https://www.topcashback.co.uk/logon?PageRequested=https%3a%2f%2fwww.topcashback.co.uk%2fHome')
+browser.get('https:XYZ.COM')
 email = browser.find_element_by_id('txtEmail')
 email.send_keys(userStr)
 password = browser.find_element_by_id('ctl00_GeckoOneColPrimary_Login_txtPassword')
